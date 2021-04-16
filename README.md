d4a112129b3ba609b056109ee46f8693c606c5ee
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-04-16 09:36:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# gcp-database-module
+
+This is an example module demonstrating a factory pattern
+for databases in GCP.
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='database',
+    version='1.0',
+    description='Factory module for GCP database',
+    author='Rosemary Wang',
+    author_email='915624+joatmon08@users.noreply.github.com',
+    packages=find_packages('.')
+)
